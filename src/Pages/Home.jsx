1fb1a7ff7be7c71d38d64988bb77b8df657b2c81
Abhinaba Mukherjee <112745548+Abhinaba1289@users.noreply.github.com>
2025-08-13 21:59:08 +0530

    added img

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
       {/* Background with Parallax Effect */}
-      {/* <motion.div 
+      <motion.div 
         className="fixed inset-0 z-0"
         initial={{ scale: 1.1 }}
         animate={{ scale: 1 }}
@@ -65,7 +65,7 @@ const Home = () => {
           className="w-full h-full object-cover"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/20"></div>
-      </motion.div> */}
+      </motion.div>
 
       {/* Content */}
       <div className="relative z-10">
